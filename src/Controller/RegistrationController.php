e21857224f173284222710560b68d2ac724a74f1
HopeFullBlack <soniq.sq@outlook.fr>
2022-12-31 00:16:12 +0100

    Creation utilisateur+connexion et deconnexion

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -48,7 +48,7 @@ class RegistrationController extends AbstractController
             // generate a signed url and email it to the user
             $this->emailVerifier->sendEmailConfirmation('app_verify_email', $user,
                 (new TemplatedEmail())
-                    ->from(new Address('contact@hopexpress.com', 'Hopexpress Contact'))
+                    ->from(new Address('hopeandprod@gmail.com', 'Hopexpress Contact'))
                     ->to($user->getEmail())
                     ->subject('Please Confirm your Email')
                     ->htmlTemplate('registration/confirmation_email.html.twig')
@@ -92,4 +92,5 @@ class RegistrationController extends AbstractController
 
         return $this->redirectToRoute('home');
     }
+    
 }
